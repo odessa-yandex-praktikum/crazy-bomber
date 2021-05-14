417e0bb732c7603ea1cee07aa9ad16b77c6a2af4
Lalalilu <39400036+lalalilulu@users.noreply.github.com>
2021-05-14 17:59:40 +0300

    S5T3: cделать страницу лидерборда (#4)
    * верстка лидерборда
    
    * добавлен импорт утилит
    
    * уменьшены изображения
    
    Co-authored-by: Anna Krivopusk <anna.krivopusk@t-systems.com>

---
+++ src/consts/index.tsx
@@ -0,0 +1,27 @@
+export const consts = {
+    gamesTitle: 'CRAZY BOMBER',
+    loginPage: {
+        login: 'Login',
+        password: 'password',
+        buttonOK: 'OK!',
+        buttonSignUp: 'sign up',
+    },
+    signinPage: {
+        email: 'email',
+        login: 'Login',
+        name: 'Name',
+        password: 'Password',
+        passwordRepeat: 'Password',
+        buttonBack: '<<',
+        buttonSignIn: 'sign in',
+    },
+    error: {
+        errorTextEmptyField: 'fill in the field',
+    },
+    leaderBoardPage: {
+        pageTitle: 'Leaderboard',
+        loginHeader: 'Login',
+        scoreHeader: 'Score',
+        linkBack: '<<',
+    },
+};
