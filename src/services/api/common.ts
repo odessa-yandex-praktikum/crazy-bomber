714e972216c5a0bf92066d0f1de92fdc191d5bb0
Sasha Malova <zapa-m@yandex.ru>
2021-08-30 22:33:04 +0300

    S9T50 github actions

--- src/services/api/common.ts
+++ src/services/api/common.ts
@@ -1,7 +1,10 @@
 export const processingRequest = (response: Response) => {
     if (response.status === 200) {
         return response;
-    } else {
+    }
+    if (response.status === 404) {
+        throw Error(response.statusText);
+    }   else {
         return response.json().then((result: {reason: string}) => {
             throw Error(result?.reason);
         });
